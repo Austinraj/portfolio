51367ce0b7cd1cad67150c85913d67df89318438
Austinraj <malaraustinraja25@gmail.com>
2023-12-15 13:08:32 +0530

    commit

--- austin-portfolio/src/component/Mainpage.js
+++ austin-portfolio/src/component/Mainpage.js
@@ -7,18 +7,37 @@ function Mainpage() {
     return (
         <>
             <Container fluid>
-                <h1>Hi all I'm Austin Raja 👋</h1>
-                <p>A passionate Frontend Developer🚀having an experience of building Web applications with HTML/CSS/JavaScript/Reactjs and somer other cool libraries and frameworks</p>
-                <Row>
-                <Col xs lg="1"><a><i class="fa-brands fa-square-github fa-2xl"></i></a>
-               <a> <i class="fa-brands fa-linkedin fa-2xl " ></i></a>
+            <Row>
+            <Col xs lg="6">
+                <h2>Hi all I'm Austin Raja 👋</h2>
+                <p>A passionate Frontend Developer🚀having an experience of building Web applications with HTML/CSS/JavaScript/Reactjs and somer other cool libraries and frameworks</p>  
+                <a><i class="fa-brands fa-square-github fa-2xl"></i></a>
+                 <a><i class="fa-brands fa-linkedin fa-2xl " ></i></a>
                 <a><i class="fa-brands fa-google fa-2xl"></i></a>
-                </Col>
-                </Row>
-
+                
+                
                 <Button className="m-5">CONTACT ME </Button>
                 <Button className="m-5">SEE MY RESUME </Button>
-                <Image alt="person" />
+                </Col>
+                <Col xs lg="6">
+                <Image src="../image/WhatIDo.avif" width={"570"} height={"500"} alt="person" />
+                </Col>
+                </Row>
+            </Container>
+            <Container>
+                <Row>
+                    <Col xs lg="6">
+                    <Image src="" alt="image" />
+                    </Col>
+                    <Col xs lg="6">
+                        <h2>What i do</h2>
+                        <p>CRAZY FRONTEND DEVELOPER WHO WANT TO EXPLORE EVERY </p>
+                    </Col>
+
+
+                </Row>
+                
+
             </Container>
         </>
     )
